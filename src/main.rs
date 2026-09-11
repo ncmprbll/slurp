d2b5f06050aa3522e0a55dc4977773c6ff0a7ab0
ncmprbll <ncmprbll@gmail.com>
2026-09-11 21:00:16 +0300

    Added named variables for proper distinction

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{fs, io::Read, process::exit};
 
 const BUFFER_SIZE: u64 = 1 << 14;
+const REGEX: &str = r#"(?s)href="\/matches\/\d+?">(.+?)<.+?data-time-ago="(\d+?)".+?((?:R\d{1,3}|Round \?) · \d\d:\d\d\.\d\d).+?data-report-message-content="(.*?)""#;
 
 fn main() {
     let file = fs::File::open("reference.html").unwrap_or_else(|err| {
@@ -15,15 +16,20 @@ fn main() {
         exit(1);
     };
 
-    let re = regex::Regex::new(r#"(?s)href="\/matches\/\d+?">(.+?)<.+?data-time-ago="(\d+?)".+?((?:R\d{1,3}|Round \?) · \d\d:\d\d\.\d\d).+?data-report-message-content="(.*?)""#).unwrap();
+    let re = regex::Regex::new(REGEX).unwrap_or_else(|err| {
+        eprintln!("regex failed: {err}");
+        exit(1);
+    });
 
     for capture in re.captures_iter(&data) {
+        let map = capture.get(1).unwrap();
+        let match_date = capture.get(2).unwrap();
+        let round_and_offset = capture.get(3).unwrap();
+        let message = capture.get(4).unwrap();
+
         println!(
             "{:?} {:?} {:?} {:?}",
-            capture.get(1),
-            capture.get(2),
-            capture.get(3),
-            capture.get(4)
+            map, match_date, round_and_offset, message
         );
     }
 
