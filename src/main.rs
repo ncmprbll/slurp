86a6561b30817ca7a08dbbbcc94247fb8c3a43c2
ncmprbll <ncmprbll@gmail.com>
2026-09-11 20:11:31 +0300

    Added a buffered reader

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::{fs, io::Read, process::exit};
+
+const BUFFER_SIZE: u64 = 1 << 14;
+
 fn main() {
-    println!("Hello, world!");
+    let file = fs::File::open("reference.html").unwrap_or_else(|err| {
+        eprintln!("Failed to open the file: {err}");
+        exit(1);
+    });
+    let mut limited_reader = file.take(BUFFER_SIZE);
+    let mut data = String::new();
+
+    if let Err(err) = limited_reader.read_to_string(&mut data) {
+        eprintln!("failed to read the file: {err}");
+        exit(1);
+    };
+
+    println!("First line:\n{:?}", data.lines().next());
 }
